e65f963cb1bb08e36c8a481d977db1f941987cad
kévin groux <kevin.groux@edu.univ-fcomte.fr>
2020-10-01 09:40:37 +0200

    Afficher liste voiture

--- kevin-theme-underscore/archive-voiture.php
+++ kevin-theme-underscore/archive-voiture.php
@@ -1,1 +1,23 @@
-<?php
+<?php get_header(); ?>
+    <div class="voiture">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <article class="voiture">
+                    <?php the_post_thumbnail( 'thumbnail' ); ?>
+                    <h1 class="title">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title(); ?>
+                        </a>
+                    </h1>
+                    <div class="content">
+                        <?php get_field('dimension'); ?>
+                        <?php get_field('couleur'); ?>
+                        <?php get_field('cylindre'); ?>
+                        <?php get_field('annee_model'); ?>
+                        <?php get_field('marque'); ?>
+                    </div>
+                </article>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+<?php get_footer(); ?>
